174a7b45067c5d625401f363a9396cf3a35f8afd
Phillip Hoff <phillip@orst.edu>
2024-05-31 15:29:47 -0700

    Revert using statement.
    Signed-off-by: Phillip Hoff <phillip@orst.edu>

--- Blazorade.Mermaid/Components/MermaidDiagram.razor.cs
+++ Blazorade.Mermaid/Components/MermaidDiagram.razor.cs
@@ -7,7 +7,6 @@ using System.ComponentModel;
 using System.Data;
 using System.Linq;
 using System.Text;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Blazorade.Mermaid.Components
